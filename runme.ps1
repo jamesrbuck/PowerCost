eb6c2af96591a8fec8bfd8e77f1a44012a0b1ba7
James Buck <james-buck@outlook.com>
2025-11-17 14:08:49 -0800

    Updated runme.ps1 for venv

--- runme.ps1
+++ runme.ps1
@@ -2,20 +2,20 @@
 
 # Path to your project root (adjust as needed)
 $projectSrcRoot = "D:\git\powercost"
-$projecExectRoot = "D:\u\apps\powercost"
+$projectExecRoot = "D:\u\apps\powercost"
+$logDir  = Join-Path $projectExecRoot "logs"
 
-# Full path to the venv pythonw.exe
+$runme_log = Join-Path $logDir "runme.txt"
+Start-Transcript -Path $runme_log -Append
+
 $venvPython = Join-Path $projectSrcRoot "venv\Scripts\pythonw.exe"
-
-# Runtime parameters
 $project = "-m powercost_project"
 $iniFile = Join-Path $projectSrcRoot "ponderosa_electricity_usage.ini"
-$logDir  = Join-Path $projecExectRoot "logs"
 
 # Startup Log (Running output is in a different log)
 if (!(Test-Path $logDir)) { New-Item -ItemType Directory -Path $logDir | Out-Null }
-$log_start_stdout = Join-Path $logDir "powercost_stdout.txt"
-$log_start_stderr = Join-Path $logDir "powercost_stderr.txt"
+$log_start_stdout = Join-Path $logDir "runme_stdout.txt"
+$log_start_stderr = Join-Path $logDir "runme_stderr.txt"
 
 # Build process options
 $processOptions = @{
@@ -28,10 +28,19 @@ $processOptions = @{
     RedirectStandardError = $log_start_stderr
 }
 
+Write-Output "projectSrcRoot   = $projectSrcRoot"
+Write-Output "projectExecRoot  = $projectExecRoot"
+Write-Output "venvPython       = $venvPython"
+Write-Output "Project          = $project"
+Write-Output "iniFile          = $iniFile"
+Write-Output "logDir           = $logDir"
+Write-Output "log_start_stdout = $log_start_stdout"
+Write-Output "log_start_stderr = $log_start_stderr"
+Write-Output " "
+
 # Launch the process
 $process = Start-Process @processOptions
 $mypid = $process.Id
 
+Write-Output "Process Options = $processOptions\n"
 Write-Output "Process started with PID = $mypid"
-Write-Output "==> Press Enter to continue..."
-Read-Host
